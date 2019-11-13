a87e570873999263732b72bfd464b445b31b5f8b
Pavel Nagornyuk <p.nagornyuk@cleack.com>
2019-11-13 23:36:21 +0200

    added webpack-dev-server

--- src/web/frontend/main.tsx
+++ src/web/frontend/main.tsx
@@ -7,11 +7,9 @@ const App = () => {
     const [counter, setCounter] = useState(0);
 
     return (
-        <div>
-            <div className="App">
-                <h1>{counter}</h1>
-                <button onClick={() => setCounter(counter + 1)}>Press me</button>
-            </div>
+        <div className="App">
+            <h1>{counter}</h1>
+            <button onClick={() => setCounter(counter + 1)}>Press me</button>
         </div>
     )
 };
