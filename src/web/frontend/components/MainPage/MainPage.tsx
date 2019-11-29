dc56536ac3fc5d982b4969f7651379e9d530dc1a
khamraeva <alinakhamraeva89@gmail.com>
2019-11-29 22:08:41 +0200

    bugfix

--- src/web/frontend/components/MainPage/MainPage.tsx
+++ src/web/frontend/components/MainPage/MainPage.tsx
@@ -7,7 +7,7 @@ import './mainpage.scss';
 export default class MainPage extends Component {
     render() {
         return (
-            <div className='container'>
+            <div className='main-page-container'>
                 <img className='unicorn' src={require('./UNICORN.png')} alt='unicorn'/>
                 <Header />
                 <Menu />
