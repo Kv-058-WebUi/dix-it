2e35f3f7a7be85ed29affef304cc81f517b1f37f
Pavel Nagornyuk <p.nagornyuk@cleack.com>
2019-12-05 15:42:17 +0200

    added preloader to lobby. Waiting for DB

--- src/web/frontend/components/Lobby/Table.tsx
+++ src/web/frontend/components/Lobby/Table.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import {Row, roomParams} from './Row'
+import Preloader from "./Preloader";
 
 interface tableState {
     list: roomParams[];
     instantList: roomParams[];
+    done: boolean
 }
 
 export default class Table extends React.Component<any, tableState> {
@@ -139,12 +141,22 @@ export default class Table extends React.Component<any, tableState> {
                 playersMax: 7
             }
         ],
-        instantList: []
+        instantList: [],
+        done: false
     };
 
     componentWillMount = () => {
         this.setState({instantList: this.state.list})
     };
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                done: true
+            })
+        }, 2000)
+    };
+
 
     compareBy(key: any) {
         return function (a: any, b: any) {
@@ -182,39 +194,50 @@ export default class Table extends React.Component<any, tableState> {
     render() {
         return (
             <div className={'lobby__table-wrapper'}>
-                <div className={'lobby__table-row head'}>
-                    <div className={'lobby__table-cell creator'}>
-                        <p className={'lobby__table-text'} onClick={() => this.sortBy('creator')}>Creator</p>
-                    </div>
-                    <div className={'lobby__table-cell room-name'}>
-                        <p className={'lobby__table-text'} onClick={() => this.sortBy('name')}>Room name</p>
-                    </div>
-                    <div className={'lobby__table-cell access'}>
-                        <p className={'lobby__table-text'} onClick={() => this.sortBy('locked')}>
-                            public
-                        </p>
-                    </div>
-                    <div className={'lobby__table-cell players'}>
-                        <p className={'lobby__table-text'} onClick={() => this.sortByPlaces('playersCur', 'playersMax')}>Players</p>
-                    </div>
-                    <div className={'lobby__table-cell btn'}/>
-                </div>
-                <div className={'lobby__table'}>
-                    {
-                        this.state.list.map((room: roomParams, i: number) => {
-                                return (
-                                    <Row {...room} key={i}/>
-                                )
-                            }
-                        )
-                    }
-                </div>
-                <div className={'lobby__filter'}>
-                    <div className={'lobby__filter-text'}>
-                        <input className={'lobby__filter-input'} onChange={this.filterRooms} type="text"
-                               placeholder={'Search...'}/>
-                    </div>
-                </div>
+
+                {
+                    !this.state.done ? (
+                        <Preloader/>
+                    ) : (
+                        <div>
+                            <div className={'lobby__table-row head'}>
+                                <div className={'lobby__table-cell creator'}>
+                                    <p className={'lobby__table-text'} onClick={() => this.sortBy('creator')}>Creator</p>
+                                </div>
+                                <div className={'lobby__table-cell room-name'}>
+                                    <p className={'lobby__table-text'} onClick={() => this.sortBy('name')}>Room name</p>
+                                </div>
+                                <div className={'lobby__table-cell access'}>
+                                    <p className={'lobby__table-text'} onClick={() => this.sortBy('locked')}>
+                                        public
+                                    </p>
+                                </div>
+                                <div className={'lobby__table-cell players'}>
+                                    <p className={'lobby__table-text'} onClick={() => this.sortByPlaces('playersCur', 'playersMax')}>Players</p>
+                                </div>
+                                <div className={'lobby__table-cell btn'}/>
+                            </div>
+                            <div className={'lobby__table'}>
+                                {
+                                    this.state.list.map((room: roomParams, i: number) => {
+                                            return (
+                                                <Row {...room} key={i}/>
+                                            )
+                                        }
+                                    )
+                                }
+                            </div>
+                            <div className={'lobby__filter'}>
+                                <div className={'lobby__filter-text'}>
+                                    <input className={'lobby__filter-input'} onChange={this.filterRooms} type="text"
+                                           placeholder={'Search...'}/>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+
+
+
             </div>
         )
     }
