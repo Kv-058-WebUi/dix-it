7803396295aea50fa9d84ca9dc2e7eebaddf0b5f
GenKozh <gen.kozh@gmail.com>
2019-12-16 20:34:51 +0200

    productin mode webpack configuration

---
+++ src/web/web.prod.ts
@@ -0,0 +1,72 @@
+import express from "express";
+import http from "http";
+import path from "path";
+import RoomController from "./backend/controllers/room.controller";
+import Controller from './backend/interfaces/controller.interface';
+import AuthenticationController from "./backend/authentication/authentication.controller";
+import bodyParser from "body-parser";
+import { UserController } from "./backend/controllers/user.controller";
+import cors from "cors";
+
+class App {
+  public app: express.Application;
+  // Express app initialization
+  constructor() {
+    this.app = express();
+    this.initializeMiddlewares();
+    this.initializeApp();
+  }
+  public initializeApp() {
+    // Template configuration
+    this.app.set("view engine", "ejs");
+    this.app.set("views", "public");
+
+    // Static files configuration
+    this.app.use("/assets", express.static(path.join(__dirname, "frontend")));
+
+    // Controllers
+    this.app.use('/api', new RoomController().router)
+    this.app.use('/api', new AuthenticationController().router)
+    this.app.use('/api', new UserController().router)
+    this.app.get("/*", (req, res) => {
+      res.render("index");
+    });
+  }
+  public listen() {
+    this.app.listen(process.env.PORT, () => {
+      console.log(`App listening on the port ${process.env.PORT}`);
+    });
+  }
+
+  public getServer() {
+    return this.app;
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(bodyParser.json());
+    this.app.use(cors());
+  }
+}
+
+// Start function
+// export const start = (port: number): Promise<void> => {
+//     const server = http.createServer(app);
+
+//     return new Promise<void>((resolve, reject) => {
+//         server.listen(port, resolve);
+//     });
+// };
+
+
+
+export const start = (port: number): Promise<void> => {
+  return new Promise<void>((resolve, reject) => {
+    port = Number(process.env.PORT);
+    const server = new App().app.listen(port, function () {
+      console.log(`Listening on port ${port}`);
+
+
+    });
+  });
+};
+
---
+++ webpack.config.prod.js
@@ -0,0 +1,77 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackRootPlugin = require('html-webpack-root-plugin');
+let path = require('path');
+
+module.exports = {
+    mode: "production",
+
+    entry: {
+        main: "./src/web/frontend/main.tsx",
+    },
+
+    output: {
+        filename: "[name].bundle.js",
+        chunkFilename: '[name].chunk.js',
+        path: __dirname + "/dist/web/frontend",
+        publicPath: "/assets/"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+            },
+
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/'
+                        }
+                    }
+            ]
+            },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {enforce: "pre", test: /\.js$/, loader: "source-map-loader"},
+            {
+                test: /\.(s*)css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        },
+        usedExports: true
+    },
+    plugins: [new HtmlWebpackPlugin(), new HtmlWebpackRootPlugin()]
+};
