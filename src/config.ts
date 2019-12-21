c37ef3a6f8e5038d4c2091ab61cb72edbdc2ff8b
takvol <cyplarix@gmail.com>
2019-12-21 22:51:38 +0200

    hardcoding env variables

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost';
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost';
-export const BACKEND_PORT = parseInt(process.env.BACKEND_PORT || "8000");
+export const BACKEND_PORT = parseInt(process.env.BACKEND_PORT || "5000");
 export const FRONTEND_PORT = parseInt(process.env.FRONTEND_PORT || "3000");
 export const SERVER_PORT = parseInt(process.env.PORT || "8000");
 export const DB_PORT = parseInt(process.env.DB_PORT || "5432");
