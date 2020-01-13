2320d2601a7fb1f64204ebb94587db2abd880f0d
Alina Hulkovska <alina_hulkovska@Alinas-MacBook-Pro.local>
2020-01-13 15:12:17 +0200

    gallery component

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
---
+++ admin/src/app/gallery/gallery.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { CARDS } from '../cardDeck';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
+})
+export class GalleryComponent implements OnInit {
+  cards = CARDS;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
