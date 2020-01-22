d90fce5741715b0f096856268708d99a72eef79f
Vlad Terniuk <vlad.terniuk@gmail.com>
2020-01-22 12:54:03 +0200

    Fixing Gallery component conection in ngModules

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
     UploadCardComponent,
     AdminComponent,
     ReviewCardsListComponent,
-    ReviewCardComponent
+    ReviewCardComponent,
     GalleryComponent
   ],
   imports: [
