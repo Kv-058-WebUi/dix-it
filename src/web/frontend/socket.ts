56088f111d1c36bff113fb404b09f9c8c2f5022f
takvol <swjk2jo@gmail.com>
2020-01-23 21:48:44 +0200

    socket build conf

--- src/web/frontend/socket.ts
+++ src/web/frontend/socket.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io(process.env.SERVER_URL+':'+process.env.SERVER_PORT);
-// const socket = io(`${process.env.CLIENT_URL}`); //for build server
+// const socket = io(process.env.SERVER_URL+':'+process.env.SERVER_PORT);
+const socket = io(`${process.env.CLIENT_URL}`); //for build server
 
 export default socket;
